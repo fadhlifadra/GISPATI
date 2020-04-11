ce915a88c0b3e31e7b7b746c712804410aba8266
Muhammad Fadhli Hisyam <38355018+fadhlifadra@users.noreply.github.com>
2020-04-12 00:48:48 +0700

    cek

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,15 +4,12 @@ class Home extends CI_Controller{
  
 	function __construct(){
 		parent::__construct();
-<<<<<<< HEAD
 		$this->load->library('session');
 		$this->load->model('web');
-=======
 	
 		if($this->session->userdata('status') != "login"){
 			redirect(base_url("login"));
 		}
->>>>>>> aa8cc06ade12b04d946f4533dea126e3edf97a71
 	}
 	public function index()
 	{
